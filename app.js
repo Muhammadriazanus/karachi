5f4b6b62c03a23c09a67c022122b1650ca9fe1ab
muhammadriazanus <riazanus568@gmail.com>
2023-02-06 08:06:10 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,101 @@
+// var city = prompt("Enter your city name")
+// if (city == city) {
+//     alert("welcome to" + " " + city +"of light")
+    
+// }
+// var gender = prompt("enter the gender")
+// var male = prompt("enter the male name")
+// if(gender == male){
+//     alert("good morning sir")
+// }
+// var gender = prompt("enter the gender")
+// var female = prompt("enter female")
+// if(gender == female){
+// //     alert("good morning mam")
+// var singnalColorName = prompt("enter the signal name:")
+// if(singnalColorName == "Red"){
+//     alert("stop please")
+// }
+// if(singnalColorName == "yellow"){
+//     alert("ready to move now")
+// }
+// if(singnalColorName == "green"){
+//     alert("move move")
+// }
+//  ------------- question no 4
+// var program = prompt("Enetr the fuel the car")
+
+// if(program > 0.25){
+//     alert("fuel the faul in the a car")
+// }
+//  ------------ Qyestion no 5------
+var total = 300;
+var mark = 219;
+var percentage = mark/total*100;
+alert(percentage)
+var k = prompt("Enter the grade of student")
+// if(k >= 80){
+//     alert("A - ONE")
+
+// }
+// if(k >= 70){
+//     alert("A")
+// }
+// if(k >= 60){
+//     alert("B")
+// }
+// if(k<=60){
+//     alert("fail")
+// }
+//     QUESTION NO 9 ----
+// var evenNumber =  prompt("Enter a numeber")
+// var m = evenNumber/2
+// if(evenNumber%2==0){
+    // alert("it is the even number")
+// }else{
+    // alert("its is the odd number")
+// }
+//  QUESTION NO 10
+// var whether = prompt("enter the number divisible by 3")
+// if(whether%3 == 0){
+    // alert("the number is divisible by 3")
+// }
+// //   QUESTION NO 10 
+// var temperature = prompt("Enter the temperature")
+// if(temperature > 40){
+//     alert("its is to hot of outside")
+// }
+// if(temperature > 30){
+//     alert("the weather today is normanl")
+// }
+// if(temperature > 20){
+//     alert("today wether is cool")
+// }
+// if(temperature > 10){
+//     alert("OMG! today weather is so cool")
+// }
+var secretNumber = prompt("user the guess the secret number")
+if(secretNumber == 10){
+    alert("Bingo! correct answer")
+}else{
+    alert("close enought to the correct answer")
+}
+//  QUESTION NO 11-----
+var num1 = +prompt("Enter the number of")
+var opt = prompt("Enetr the operator of ")
+var num2 = +prompt("Enter the second number of ")
+if(opt == "+"){
+    alert(num1 + num2)
+}
+if(opt == "-"){
+    alert(num1 - num2)
+}
+if(opt == "*"){
+    alert(num1*num2)
+}
+if(opt == "/"){
+    alert(num1 / num2)
+}
+if(opt == "%"){
+    alert(num1 % num2)
+}
---
+++ assigmentlast.js
@@ -0,0 +1,29 @@
+// var number = prompt("Enetr a number")
+// // var string = prompt("Enter the string")
+// if(number == 65 ){
+//     alert("Upper casae of A")
+// }else if(number == 90 ){
+//     alert("upper case of Z")
+// }else if(number == 97){
+//     alert("lower case a")
+// }else if(number == 122){
+//     alert("lower case z")
+// }
+// var number1 = prompt("Enter the integer number 1")
+// var number2 = prompt("Enter the integer number 2")
+// if (number1 > number2) {
+//     alert("dispaly the larger one")   
+// }else if(number1 == number2){
+//     alert("same number")
+
+// }
+var wetherNunber = prompt("Enter the number")
+if(wetherNunber == "positive"){
+    alert("The positive number")
+}else if(wetherNunber == "negative"){
+    alert("the negative number")
+
+}else if(wetherNunber == "zero"){
+    alert("the number is zero")
+
+}
